d1d58ef4fe2751d07c2f3ce4cc5d565a0734510d
FCG <admin@iotlet.cn>
2021-07-08 13:35:48 +0800

    获取网关信息 统一返回 EdgeInfoDetail

--- src/main/java/org/jetlinks/edge/core/web/EdgeOperationsController.java
+++ src/main/java/org/jetlinks/edge/core/web/EdgeOperationsController.java
@@ -3,7 +3,7 @@ package org.jetlinks.edge.core.web;
 import org.hswebframework.web.authorization.annotation.Resource;
 import org.hswebframework.web.authorization.annotation.ResourceAction;
 import org.jetlinks.edge.core.EdgeOperations;
-import org.jetlinks.edge.core.entity.EdgeInfoEntity;
+import org.jetlinks.edge.core.entity.EdgeInfoDetail;
 import org.jetlinks.edge.core.monitor.EdgeRunningState;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -45,7 +45,7 @@ public class EdgeOperationsController {
 
     @GetMapping(value = "/{deviceId}/detail")
     @ResourceAction(id = "device-info", name = "网关详情")
-    public Mono<EdgeInfoEntity> edgeDeviceInfo(@PathVariable String deviceId) {
+    public Mono<EdgeInfoDetail> edgeDeviceInfo(@PathVariable String deviceId) {
         return edgeOperations.edgeDeviceInfo(deviceId);
     }
 
