40d571f234e715f1ef7951981f294d8c469a0f11
yangbo <yangbo_1@126.com>
2022-10-17 11:48:34 +0800

    调整[接入IOT]相关字段

--- src/main/java/org/jetlinks/edge/core/entity/EdgeInfoDetail.java
+++ src/main/java/org/jetlinks/edge/core/entity/EdgeInfoDetail.java
@@ -27,11 +27,11 @@ public class EdgeInfoDetail {
     @Schema(description = "固件版本")
     private String firmwareVer;
 
-    @Schema(description = "操作系统")
-    private String osInfo;
-
-    @Schema(description = "网关地理位置信息")
-    private String geoAdder;
+//    @Schema(description = "操作系统")
+//    private String osInfo;
+//
+//    @Schema(description = "网关地理位置信息")
+//    private String geoAdder;
 
     @Schema(description = "网关在平台的产品id")
     private String productId;
@@ -50,6 +50,12 @@ public class EdgeInfoDetail {
 
     @Schema(description = "平台端口")
     private int port;
+
+    @Schema(description = "穿透端口")
+    private int penetrationPort;
+
+    @Schema(description = "离线缓存")
+    private boolean offlineCache;
 
     @Schema(description = "是否连接平台")
     private boolean connected;
